44a66f1d9f38242021fdf44a6ced8985d283e5be
Zan Skamljic <zan.skamljic@equaleyes.com>
2025-03-18 08:21:43 +0100

    feat: enable static lines, habamax theme

--- init.lua
+++ init.lua
@@ -1,6 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,9 +1,9 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  --{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      colorscheme = "habamax",
     },
   },
 }
